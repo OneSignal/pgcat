a7890c9fc28dc6c3ff77e7e131c6e764f0b155db
Jose Fernandez (magec) <joseferper@gmail.com>
2022-12-07 13:18:42 +0100

    Add dns_cache so server addresses are cached and invalidated when DNS changes.
    Adds a module to deal with dns_cache feature. It's
    main struct is CachedResolver, which is a simple thread safe
    hostname <-> Ips cache with the ability to refresh resolutions
    every `dns_max_ttl` seconds. This way, a client can check whether its
    ip address has changed.

--- src/config.rs
+++ src/config.rs
@@ -163,6 +163,12 @@ pub struct General {
     #[serde(default)] // False
     pub log_client_disconnections: bool,
 
+    #[serde(default)] // False
+    pub dns_cache_enabled: bool,
+
+    #[serde(default = "General::default_dns_max_ttl")]
+    pub dns_max_ttl: u64,
+
     #[serde(default = "General::default_shutdown_timeout")]
     pub shutdown_timeout: u64,
 
@@ -201,6 +207,10 @@ impl General {
         60000
     }
 
+    pub fn default_dns_max_ttl() -> u64 {
+        30
+    }
+
     pub fn default_healthcheck_timeout() -> u64 {
         1000
     }
@@ -228,6 +238,8 @@ impl Default for General {
             ban_time: Self::default_ban_time(),
             log_client_connections: false,
             log_client_disconnections: false,
+            dns_cache_enabled: false,
+            dns_max_ttl: Self::default_dns_max_ttl(),
             autoreload: false,
             tls_certificate: None,
             tls_private_key: None,
