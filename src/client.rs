2a1e0313c46f56c168cb0955a624c6c99b4c5d60
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2025-12-31 10:09:50 +0000

    Log a dropped cancellation query as an error

--- src/client.rs
+++ src/client.rs
@@ -845,7 +845,10 @@ where
 
                     // The client doesn't know / got the wrong server,
                     // we're closing the connection for security reasons.
-                    None => return Ok(()),
+                    None => {
+                        error!("An unknown cancel query was received and ignored. Was it sent to the wrong server?");
+                        return Ok(());
+                    }
                 }
             };
 
